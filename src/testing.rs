fba4c1d45896542c3aa67c670f6efaceabd516b1
rickbeeloo <r.beeloo@outlook.com>
2024-03-14 14:40:54 +0100

    heaps + less RAM

--- src/testing.rs
+++ src/testing.rs
@@ -7,13 +7,11 @@ use std::io::BufRead;
 use clap::{Arg, App};
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use fuzzywuzzy::fuzz;
 
 const MAGNITUDE: i32 = 2_i32.pow(30);
 
 #[derive(Debug, PartialEq, Eq)]
 struct Entry {
-    ref_index: u32,
     coverage: i32,
     length: i32,
 }
@@ -21,7 +19,7 @@ struct Entry {
 impl Ord for Entry {
     fn cmp(&self, other: &Self) -> Ordering {
         match other.coverage.cmp(&self.coverage) {
-            Ordering::Equal => self.length.cmp(&other.length), // Compare length if coverage is equal
+            Ordering::Equal => other.length.cmp(&self.length), // Compare length if coverage is equal
             ord => ord, // Return coverage ordering if not equal
         }
     }
@@ -44,14 +42,14 @@ fn update_heap(heap: &mut BinaryHeap<Entry>, entry: Entry) {
     }
 }
 
-fn transform(ref_index: u32, coverage: u32, length: i32) -> Entry {
-    Entry{ref_index, coverage: (coverage as i32) - MAGNITUDE, length: length - MAGNITUDE}
+fn transform(coverage: i32, length: i32) -> Entry {
+    Entry{ coverage: coverage - MAGNITUDE, length: length - MAGNITUDE}
 }
 
-fn reverse_transform(entry: Entry) -> (u32, i32, i32) {
+fn reverse_transform(entry: Entry) -> (i32, i32) {
     let coverage: i32 = MAGNITUDE + entry.coverage;
     let length: i32= MAGNITUDE + entry.length;
-    (entry.ref_index, coverage, length)
+    (coverage, length)
 }
 
 fn read_lines_to_uint8_vector(file_path: &str) -> Result<Vec<Vec<u8>>, std::io::Error> {
@@ -87,79 +85,30 @@ fn index_queries(queries: &[Vec<u8>]) -> HashMap<(u8, u8), HashMap<usize, u32>> 
 }
 
 
-fn heuristic_filter(index: &HashMap<(u8, u8), HashMap<usize, u32>>, refs: &[Vec<u8>], query_lens: &Vec<u32>, cov_vector: &mut Vec<u32>, heaps: &mut Vec<BinaryHeap<Entry>> ) {
-    refs.iter().enumerate().for_each(|(j, r)| {
+fn query(index: &HashMap<(u8, u8), HashMap<usize, u32>>, refs: &[Vec<u8>], cov_vector: &mut [Vec<u32>], len_vector: &mut [Vec<u32>] ) {
+    refs.iter().enumerate().for_each(|(j, ref_)| {
         
-        // Reset coverage vec
-        cov_vector.iter_mut().for_each(|m| *m = 0);
-
         // Show the progress
         if j % 100 == 0 {
             println!("Proccessed: {}/{}", j, refs.len());
         }
         
-        // Fill coverage matrix
-        for bigram in generate_bigrams(r) {
+        // Show 
+        for bigram in generate_bigrams(ref_) {
             if let Some(entry) = index.get(&bigram) {
+
                 // Update Q * R match
                 for (query_id, count) in entry {
-                    cov_vector[*query_id] += count - 1; // Consider query count to scale
+                    cov_vector[query_id] += count;
                 }
             }
         }
-        
-        // For each query we now have the coverage with the current ref
-        // we can push these to the heaps
-        cov_vector.iter().enumerate().for_each(|(query_index, coverage)|{
-            let q_len = query_lens[query_index] as i32;
-            let length_difference = (r.len() as i32 - q_len).abs();
-            update_heap(&mut heaps[query_index], transform(j as u32, *coverage, length_difference));
-        }) 
     });
 }
 
 
-fn fuzz_pass(heaps: &mut Vec<BinaryHeap<Entry>>, queries: &[Vec<u8>], refs: &[Vec<u8>], cut_off: u8) {
-    for (bytes, heap) in queries.iter().zip(heaps.iter_mut()) {
-        let query_string = String::from_utf8_lossy(bytes);
-
-        println!("Query: {}", query_string);
-        
-        let (max_match, max_score, max_len) = find_max_match(heap, refs, &query_string, cut_off);
-
-        println!("Best match: {}", max_match);
-        println!(" ---- END ----")
-    }
-}
-
-fn find_max_match(heap: &mut BinaryHeap<Entry>, refs: &[Vec<u8>], query_string: &str, cut_off: u8) -> (String, u8, usize) {
-    let mut max_score = 0;
-    let mut max_match = String::new();
-    let mut max_len = 0;
-
-    while let Some(item) = heap.pop() {
-        let (ref_index, coverage, l) = reverse_transform(item);
-        let ref_bytes = &refs[ref_index as usize];
-        let ref_string = String::from_utf8_lossy(ref_bytes);
-
-        let fuzz_r = fuzz::partial_ratio(&ref_string, query_string);
-        // println!("{} : {}: {} ", query_string, ref_string, fuzz_r);
-
-        if fuzz_r >= cut_off && (fuzz_r > max_score || (max_score == fuzz_r && ref_bytes.len() > max_len)) {
-            max_match = ref_string.into_owned(); 
-            max_score = fuzz_r;
-            max_len = ref_bytes.len();
-        }
-    }
-
-    (max_match, max_score, max_len)
-}
-
-
-
 
 fn main() {
-
     let matches = App::new("Matrix Builder")
         .arg(Arg::with_name("query")
             .help("Path to the query file")
@@ -169,16 +118,11 @@ fn main() {
             .help("Path to the reference file")
             .required(true)
             .index(2))
-        .arg(Arg::with_name("cutoff")
-            .help("Fuzzing score cut-off")
-            .required(true)
-            .index(3))
         .get_matches();
 
     
     let query_path = matches.value_of("query").unwrap();
     let ref_path = matches.value_of("reference").unwrap();
-    let cut_off: u8 = matches.value_of("cutoff").unwrap().parse().expect("Cut off not a number");
 
     println!("Reading data...");
     let query_vector = read_lines_to_uint8_vector(query_path).expect("Error reading query");
@@ -188,22 +132,15 @@ fn main() {
     let index = index_queries(&query_vector);
 
     println!("Querying...");
+    let mut m = Array2::<u32>::zeros((ref_vector.len(), query_vector.len()));
+
     let mut cov_vector = vec![0;query_vector.len()];
-    let mut len_vector: Vec<u32> = query_vector.iter().map(|sublist| sublist.len() as u32).collect();
-    let mut heaps: Vec<BinaryHeap<Entry>> = Vec::new();
-    for _ in 0..query_vector.len() {
-        heaps.push(BinaryHeap::new());
-    }
-
-    heuristic_filter(&index, &ref_vector, &len_vector, &mut cov_vector, &mut heaps);
-
-    // for heap in heaps {
-    //     println!("{:?}", heap);
-    // }
-
-    fuzz_pass(&mut heaps, &query_vector, &ref_vector, cut_off);
+    len mut len_vector = vec![0;query_vector.len()];
+    get_coverage_matrix(&index, &ref_vector, &mut cov_vector, &mut len_vector);
     
     println!("Done!");
+    println!("{:?}", m);
+
 }
 
 // fn main() {
@@ -216,8 +153,3 @@ fn main() {
 
 //     println!("{:?}", mat);
 // }
-
-
-// a yellow banana from the store
-// vanilla
-// peanutbutter
