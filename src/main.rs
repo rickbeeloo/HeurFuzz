600ba6ee68d3598795371e5800006c133a4c3d25
rickbeeloo <r.beeloo@outlook.com>
2024-03-14 15:33:55 +0100

    bigger steps

--- src/main.rs
+++ src/main.rs
@@ -94,19 +94,25 @@ fn heuristic_filter(index: &HashMap<(u8, u8, u8), HashMap<usize, u32>>, refs: &[
         cov_vector.iter_mut().for_each(|m| *m = 0);
 
         // Show the progress
-        if j % 100 == 0 {
+        if j % 100_000 == 0 {
             println!("Proccessed: {}/{}", j, refs.len());
         }
         
         // Fill coverage matrix
-        for bigram in generate_bigrams(r) {
-            if let Some(entry) = index.get(&bigram) {
-                // Update Q * R match
-                for (query_id, count) in entry {
-                    cov_vector[*query_id] += count - 1; // Consider query count to scale
+        if r.len() > 3 {
+            for bigram in generate_bigrams(r) {
+                if let Some(entry) = index.get(&bigram) {
+                    // Update Q * R match
+                    for (query_id, count) in entry {
+                        cov_vector[*query_id] += count - 1; // Consider query count to scale
+                    }
                 }
             }
+        } else {
+            println!("Discarded: {:?}", r);
         }
+
+        
         
         // For each query we now have the coverage with the current ref
         // we can push these to the heaps
